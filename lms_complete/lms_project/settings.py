3b383d9cf14e9b1772cf321be05d1d3e30dc1a52
kumarayush07ak <kumarayush.07ak@gmail.com>
2026-02-20 21:07:51 +0530

    add external mail

--- lms_complete/lms_project/settings.py
+++ lms_complete/lms_project/settings.py
@@ -13,6 +13,10 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 from pathlib import Path
 import os
 import sys
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -151,9 +155,9 @@ LOGOUT_REDIRECT_URL = 'accounts:login'
 # Email configuration (for console in development)
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST='smtp.gmail.com'
-EMAIL_USE_TLS=True
-EMAIL_PORT=587
-EMAIL_HOST_USER='prachisonu6@gmail.com'
-EMAIL_HOST_PASSWORD='cwshmxbbsgsarhuv'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
